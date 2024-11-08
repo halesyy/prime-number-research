828ef7dcc55b33ae2cdd490c806a126d0f76ac01
Jack Hales <halesyy@gmail.com>
2024-11-08 20:08:57 +0800

    Numba optimisation - going to do more testing to try to do multivariate tweaking to find new patterns, or less vol

--- primes/expressions/valuable.py
+++ primes/expressions/valuable.py
@@ -1,7 +1,9 @@
 
 
+from time import perf_counter
+import numba
+import numpy as np
 from primes.expressions.generator import parse_expression
-
 
 def load_x_log_x_y_ex():
    ex_mv_str = "((x*a)*log(x*b,y))"
@@ -9,3 +11,22 @@ def load_x_log_x_y_ex():
    if ex is None:
       raise ValueError(f"Failed to parse expression: {ex_mv_str}")
    return ex
+
+def x_log_x_y_ex(x: float, y: float, a: float, b: float) -> float:
+   return (x * a) * (np.log(x * b) / np.log(y))
+
+@numba.njit()
+def numba_x_log_x_y_ex(x: float, y: float, a: float, b: float) -> float:
+   return (x * a) * (np.log(x * b) / np.log(y))
+
+# normal: 50x faster
+# numba: 237x faster
+
+if __name__ == "__main__":
+   ex = load_x_log_x_y_ex()
+   ex.evaluate({"x": 5, "y": 5, "a": 5, "b": 5})
+   numba_x_log_x_y_ex(5, 5, 5, 5)
+   start = perf_counter()
+   for _ in range(100000):
+      numba_x_log_x_y_ex(x=5, y=5, a=5, b=5) # this is 50x faster than the evaluation function
+   print(perf_counter() - start)
---
+++ primes/precision_miner/miner_optim.py
@@ -0,0 +1,9 @@
+
+from primes.expressions.valuable import load_x_log_x_y_ex
+
+
+def main():
+   ex = load_x_log_x_y_ex()
+
+if __name__ == "__main__":
+   main()
